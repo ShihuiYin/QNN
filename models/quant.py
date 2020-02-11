ccbcba5d7a8e4186c98079c37bf153393d5c6228
Shihui Yin - Sch Elect Comptr Syin11 Energy Engr <syin11@titanx03.ecee.dhcp.asu.edu>
2020-02-11 12:20:31 -0700

    new file:   LICENSE 	new file:   models/quant.py 	new file:   models/vgg_binary.py

---
+++ models/quant.py
@@ -0,0 +1,94 @@
+# Copyright 2020   Shihui Yin    Arizona State University
+
+# Licensed under the Apache License, Version 2.0 (the "License")
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+# THIS CODE IS PROVIDED *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY IMPLIED
+# WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
+# MERCHANTABLITY OR NON-INFRINGEMENT.
+# See the Apache 2 License for the specific language governing permissions and
+# limitations under the License.
+
+# Description: functions for weight/activation quantization and input splitting
+# Created on 02/09/2020
+import torch
+import torch.nn as nn
+from torch.autograd import Function
+
+
+def Binarize(tensor):
+    return tensor.sign()
+
+class BinarizeAct(Function):
+    @staticmethod
+    def forward(self, input):
+        self.save_for_backward(input)
+        return torch.sign(input)
+
+    @staticmethod
+    def backward(self, grad_output):
+        input = self.saved_tensors
+        grad_output[input > 1] = 0
+        grad_output[input < -1] = 0
+        return grad_output
+
+class BinarizeActLayer(nn.Module):
+    def __init__(self):
+        super(BinarizeActLayer, self).__init__()
+
+    def forward(self, x):
+        return BinarizeAct.apply(x)
+
+class HingeLoss(nn.Module):
+    def __init__(self):
+        super(HingeLoss,self).__init__()
+        self.margin=1.0
+
+    def hinge_loss(self,input,target):
+            output=self.margin-input.mul(target)
+            output[output.le(0)]=0
+            return output.mean()
+
+    def forward(self, input, target):
+        return self.hinge_loss(input,target)
+
+
+class BinarizeLinear(nn.Linear):
+
+    def __init__(self, *kargs, **kwargs):
+        super(BinarizeLinear, self).__init__(*kargs, **kwargs)
+
+    def forward(self, input):
+        if not hasattr(self.weight,'org'):
+            self.weight.org=self.weight.data.clone()
+        self.weight.data=Binarize(self.weight.org)
+        out = nn.functional.linear(input, self.weight)
+        if not self.bias is None:
+            self.bias.org=self.bias.data.clone()
+            out += self.bias.view(1, -1).expand_as(out)
+
+        return out
+
+class BinarizeConv2d(nn.Conv2d):
+
+    def __init__(self, *kargs, **kwargs):
+        super(BinarizeConv2d, self).__init__(*kargs, **kwargs)
+
+
+    def forward(self, input):
+        if not hasattr(self.weight,'org'):
+            self.weight.org=self.weight.data.clone()
+        self.weight.data=Binarize(self.weight.org)
+
+        out = nn.functional.conv2d(input, self.weight, None, self.stride,
+                                   self.padding, self.dilation, self.groups)
+
+        if not self.bias is None:
+            self.bias.org=self.bias.data.clone()
+            out += self.bias.view(1, -1, 1, 1).expand_as(out)
+
+        return out
---
+++ models/vgg_binary.py
@@ -0,0 +1,64 @@
+'''VGG11/13/16/19 in Pytorch.'''
+import torch
+import torch.nn as nn
+from quant import BinarizeAct, BinarizeConv2d, BinarizeLinear, BinarizeActLayer
+
+cfg = {
+    'VGG': [128, 128, 'M', 256, 256, 'M', 512, 512, 'M'],
+    'VGG11': [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'VGG13': [64, 64, 'M', 128, 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'VGG16': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'],
+    'VGG19': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M'],
+}
+
+
+class VGG_binary(nn.Module):
+    def __init__(self, vgg_name, fc=512):
+        super(VGG_binary, self).__init__()
+        self.features = self._make_layers(cfg[vgg_name])
+        num_maxpooling_layers = cfg[vgg_name].count('M')
+        last_conv_layer_output_dim = 512 * (4 ** (5 - num_maxpooling_layers))
+        self.classifier = nn.Sequential(
+                BinarizeLinear(last_conv_layer_output_dim, fc),
+                nn.BatchNorm1d(fc),
+                BinarizeActLayer(),
+                BinarizeLinear(fc, fc),
+                nn.BatchNorm1d(fc),
+                BinarizeActLayer(),
+                BinarizeLinear(fc, 10),
+                nn.BatchNorm1d(10)
+                )
+
+    def forward(self, x):
+        out = self.features(x)
+        out = out.view(out.size(0), -1)
+        out = self.classifier(out)
+        return out
+
+    def _make_layers(self, cfg):
+        layers = []
+        in_channels = 3
+        for x in cfg:
+            if in_channels == 3:
+                layers += [nn.Conv2d(in_channels, x, kernel_size=3, padding=1)]
+                layers += [nn.BatchNorm2d(x)]
+                in_channels = x
+            else:
+                if x == 'M':
+                    layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+                else:
+                    layers += [BinarizeActLayer()]
+                    layers += [BinarizeConv2d(in_channels, x, kernel_size=3, padding=1),
+                               nn.BatchNorm2d(x)]
+                    in_channels = x
+        layers += [BinarizeActLayer()]
+        return nn.Sequential(*layers)
+
+
+def test():
+    net = VGG('VGG11')
+    x = torch.randn(2,3,32,32)
+    y = net(x)
+    print(y.size())
+
+# test()
