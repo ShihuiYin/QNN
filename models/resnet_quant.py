7e9da6bd03e500ada2e9ce62a02486aa36557c0e
ShihuiYin <shyin@outlook>
2020-06-11 16:05:33 -0700

    modified:   models/resnet_quant.py

--- models/resnet_quant.py
+++ models/resnet_quant.py
@@ -14,7 +14,7 @@ from .quant import QuantizeConv2d, QuantizeLinear, QuantizeActLayer
 class BasicBlock_quant(nn.Module):
     expansion = 1
 
-    def __init__(self, in_planes, planes, stride=1, w_bits=1, w_H=1., a_bits=1, a_H=1.):
+    def __init__(self, in_planes, planes, stride=1, w_bits=1, w_H=1., a_bits=1, a_H=1., quant_output=True):
         super(BasicBlock_quant, self).__init__()
         self.conv1 = QuantizeConv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False,
                 n_bits=w_bits, H=w_H)
@@ -23,7 +23,7 @@ class BasicBlock_quant(nn.Module):
         self.conv2 = QuantizeConv2d(planes, planes*self.expansion, kernel_size=3, stride=1, padding=1, bias=False,
                 n_bits=w_bits, H=w_H)
         self.bn2 = nn.BatchNorm2d(planes*self.expansion)
-        self.quant_act2 = QuantizeActLayer(n_bits=a_bits, H=a_H)
+        self.quant_act2 = QuantizeActLayer(n_bits=a_bits, H=a_H) if quant_output else lambda x: x
 
         self.shortcut = nn.Sequential()
         if stride != 1 or in_planes != self.expansion*planes:
@@ -78,7 +78,7 @@ class ResNet_quant(nn.Module):
         self.conv1 = QuantizeConv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False,
                 n_bits=w_bits, H=w_H)
         self.bn1 = nn.BatchNorm2d(64)
-        self.quant_act = QuantizeActLayer(n_bits=a_bits, H=a_H)
+        self.quant_act1 = QuantizeActLayer(n_bits=a_bits, H=a_H)
         self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1, 
                 w_bits=w_bits, w_H=w_H, a_bits=a_bits, a_H=a_H)
         self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2,
@@ -86,7 +86,8 @@ class ResNet_quant(nn.Module):
         self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2,
                 w_bits=w_bits, w_H=w_H, a_bits=a_bits, a_H=a_H)
         self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2,
-                w_bits=w_bits, w_H=w_H, a_bits=a_bits, a_H=a_H)
+                w_bits=w_bits, w_H=w_H, a_bits=a_bits, a_H=a_H, quant_output=False)
+        self.quant_act2 = QuantizeActLayer(n_bits=a_bits, H=a_H)
         self.linear = QuantizeLinear(512*block.expansion, num_classes, n_bits=w_bits, H=w_H)
 
     def _make_layer(self, block, planes, num_blocks, stride, w_bits=1, w_H=1., a_bits=1, a_H=1.):
@@ -98,13 +99,14 @@ class ResNet_quant(nn.Module):
         return nn.Sequential(*layers)
 
     def forward(self, x):
-        out = self.quant_act(self.bn1(self.conv1(x)))
+        out = self.quant_act1(self.bn1(self.conv1(x)))
         out = self.layer1(out)
         out = self.layer2(out)
         out = self.layer3(out)
         out = self.layer4(out)
         out = F.avg_pool2d(out, 4)
         out = out.view(out.size(0), -1)
+        out = self.quant_act2(out)
         out = self.linear(out)
         return out
 
