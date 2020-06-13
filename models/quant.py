b802563b83cba77b3d5763c90d23ae54f7a01af4
ShihuiYin <shyin@outlook>
2020-06-12 17:01:40 -0700

    modified:   main.py 	modified:   models/quant.py

--- models/quant.py
+++ models/quant.py
@@ -11,6 +11,18 @@ def Quantize(tensor, H=1., n_bits=2):
         tensor=torch.round((torch.clamp(tensor, -H, H)+H) * (2**n_bits - 1) / (2*H)) * 2*H / (2**n_bits - 1) - H
     return tensor
 
+def Quantize2(n_bits):
+    if n_bits == 1:
+        def binarize(x, H):
+            return x.sign() * H
+        return binarize
+    k = 2**n_bits - 1.
+    def quantize(x, H):
+        with torch.no_grad():
+            xq = (torch.round((torch.clamp(x/(2*H), -0.5, 0.5) + 0.5) * k) / k - 0.5) * 2 * H
+        return xq
+    return quantize
+
 class Quantize_STE_clipped(Function):
     @staticmethod
     def forward(ctx, input, H, n_bits):
@@ -26,6 +38,23 @@ class Quantize_STE_clipped(Function):
         grad_input[abs(input) > H] = 0
         return grad_input, grad_H, None
 
+def Quantize_STE(n_bits):
+    quant = Quantize2(n_bits)
+    class Quantize_STE_clipped2(Function):
+        @staticmethod
+        def forward(ctx, input, H):
+            ctx.save_for_backward(input, H)
+            return quant(input, H)
+
+        @staticmethod
+        def backward(ctx, grad_output):
+            input, H, = ctx.saved_tensors
+            grad_input = grad_output.clone()
+            grad_H = torch.sum(quant(input/H, 1) * grad_output.clone()).clamp_(-0.001, 0.001)
+            grad_input[abs(input) > H] = 0
+            return grad_input, grad_H
+    return Quantize_STE_clipped2
+
 class Quantize_STE_identity(Function):
     @staticmethod
     def forward(ctx, input, H, n_bits):
@@ -37,8 +66,8 @@ class Quantize_STE_identity(Function):
         grad_H = torch.sum(torch.sign(input) * grad_output.clone())
         return grad_input, grad_H, None
 
-QuantizeAct = Quantize_STE_clipped
-QuantizeWeight = Quantize_STE_clipped
+#QuantizeAct = Quantize_STE_clipped2
+#QuantizeWeight = Quantize_STE_clipped2
     
 class QuantizeActLayer(nn.Module):
     def __init__(self, n_bits=2, H=1., inplace=True):
@@ -48,9 +77,10 @@ class QuantizeActLayer(nn.Module):
         self.H_init = H
         self.H = nn.Parameter(data=torch.Tensor(1),requires_grad=True)
         self.H.data = torch.tensor(self.H_init)
+        self.quantize = Quantize_STE(n_bits)
 
     def forward(self, x):
-        return QuantizeAct.apply(x, self.H, self.n_bits)
+        return self.quantize.apply(x, self.H)
 
     def extra_repr(self):
         return super(QuantizeActLayer, self).extra_repr() + 'n_bits={}'.format(self.n_bits) + ', H={}'.format(self.H)
@@ -66,9 +96,10 @@ class QuantizeLinear(nn.Linear):
         self.H = nn.Parameter(data=torch.Tensor(1), requires_grad=True)
         #self.H.data = torch.tensor(self.H_init)
         self.H.data = 2 * torch.std(self.weight.data)
+        self.quantize = Quantize_STE(self.n_bits).apply
 
     def forward(self, input):
-        out = nn.functional.linear(input, QuantizeWeight.apply(self.weight, self.H, self.n_bits))
+        out = nn.functional.linear(input, self.quantize(self.weight, self.H))
         if not self.bias is None:
             out += self.bias.view(1, -1).expand_as(out)
         return out
@@ -87,9 +118,10 @@ class QuantizeConv2d(nn.Conv2d):
         self.H = nn.Parameter(data=torch.Tensor(1), requires_grad=True)
         #self.H.data = torch.tensor(self.H_init)
         self.H.data = 2 * torch.std(self.weight.data)
+        self.quantize = Quantize_STE(self.n_bits).apply
 
     def forward(self, input):
-        out = nn.functional.conv2d(input, QuantizeWeight.apply(self.weight, self.H, self.n_bits), 
+        out = nn.functional.conv2d(input, self.quantize(self.weight, self.H), 
                 None, self.stride, self.padding, self.dilation, self.groups)
 
         if not self.bias is None:
